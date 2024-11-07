71adde17f72af11f2fba24ea872eb257ed0884e4
zzzyoonnn <dwsrh03@gmail.com>
2024-11-08 00:51:50 +0900

    refactor: InventoryController를 PosMachineController로 수정

--- src/main/java/store/controller/ConvenienceStoreController.java
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -3,11 +3,11 @@ package store.controller;
 import store.view.ConvenienceStoreView;
 
 public class ConvenienceStoreController {
-    static InventoryController inventoryController = new InventoryController();
+    static PosMachineController posMachineController = new PosMachineController();
 
     public static void open() {
         // 재고 관리하기
-        inventoryController.createInventory();
+        posMachineController.initialize();
         ConvenienceStoreView.displayWelcomeGreeting();
         // 손님 받기
         // 영수증 출력하기
