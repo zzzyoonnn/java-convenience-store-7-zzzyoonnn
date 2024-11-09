4c59a51c60eee5fddbeeda2eea8829dc4d7d2e9b
zzzyoonnn <dwsrh03@gmail.com>
2024-11-09 14:19:22 +0900

    refactor: User 객체의 userCart를 buyingInput로 수정

--- src/main/java/store/domain/User.java
+++ src/main/java/store/domain/User.java
@@ -3,21 +3,23 @@ package store.domain;
 import store.message.StaffErrorMessage;
 
 public class User {
-    private String userCart;
+    private String buyingInput;
 
     public User() {
 
     }
 
-    public User(String userCart) {
-        isValidString(userCart);
-        this.userCart = userCart;
+    public User(String buyingInput) {
+        isValidString(buyingInput);
+        this.buyingInput = buyingInput;
     }
 
-    private void isValidString(String userCart) {
+    public boolean isValidString(String userCart) {
         String regex = "^\\[([가-힣]+-\\d+)\\]+(,\\[([가-힣]+-\\d+)\\])*$";
         if (!userCart.matches(regex)) {
             throw new IllegalArgumentException(StaffErrorMessage.IS_INVALID_STRING.getFormattedMessage());
         }
+
+        return true;
     }
 }
