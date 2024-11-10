9be6cf82fd8f743bcb317176bca7512c524df636
zzzyoonnn <dwsrh03@gmail.com>
2024-11-10 17:54:43 +0900

    feat: 프로모션 적용을 취소할지에 대한 메시지를 출력하는 displayAskNoPromotionPayment 구현

--- src/main/java/store/view/StaffView.java
+++ src/main/java/store/view/StaffView.java
@@ -1,5 +1,6 @@
 package store.view;
 
+import static store.message.StaffMessage.ASK_NO_PROMOTION_PAYMENT;
 import static store.message.StaffMessage.REQUEST_PRODUCT_NAME_AND_QUANTITY;
 import static store.message.StaffMessage.WELCOME_GREETING;
 
@@ -10,5 +11,9 @@ public class StaffView {
 
     public static void displayRequestProductNameAndQuantity() {
         System.out.println(REQUEST_PRODUCT_NAME_AND_QUANTITY.getMessage());
+    }
+
+    public static void displayAskNoPromotionPayment() {
+        System.out.println(ASK_NO_PROMOTION_PAYMENT.getMessage());
     }
 }
