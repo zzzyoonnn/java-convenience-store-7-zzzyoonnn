78d2ea72ea5979a85e228ea2f06990d9da4e0cec
zzzyoonnn <dwsrh03@gmail.com>
2024-11-11 14:19:16 +0900

    feat: 영수증을 출력할 때 사용하는 Receipt 객체 구현

--- src/main/java/store/domain/Receipt.java
+++ src/main/java/store/domain/Receipt.java
@@ -1,4 +1,15 @@
 package store.domain;
 
 public class Receipt {
+    private String productName;
+    private int productPrice;
+    private int productQuantity;
+    private int promotionQuantity;
+
+    public Receipt(String productName, int productPrice, int productQuantity, int promotionQuantity) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.promotionQuantity = promotionQuantity;
+    }
 }
