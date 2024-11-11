33097fc5c80877a5a6a90d56c1e3c9f267ab0eae
zzzyoonnn <dwsrh03@gmail.com>
2024-11-11 16:47:06 +0900

    feat: 구매목록에 존재하지 않는 제품이라면 영수증에 추가하는 addReceipt 구현

--- src/main/java/store/domain/ReceiptManagement.java
+++ src/main/java/store/domain/ReceiptManagement.java
@@ -9,18 +9,9 @@ public class ReceiptManagement {
         receiptMap = new HashMap<>();
     }
 
-    public void addOrUpdateReceipt(String productName, int productPrice, int productQuantity, int promotionQuantity) {
-        if (receiptMap.containsKey(productName)) {
-            // 기존 데이터를 수정
-            Receipt existingReceipt = receiptMap.get(productName);
-            existingReceipt.setProductPrice(productPrice);
-            existingReceipt.setProductQuantity(productQuantity);
-            existingReceipt.setPromotionQuantity(promotionQuantity);
-        } else {
-            // 새로운 데이터를 추가
-            Receipt newReceipt = new Receipt(productName, productPrice, productQuantity, promotionQuantity);
-            receiptMap.put(productName, newReceipt);
-        }
+    public void addReceipt(String productName, int productPrice, int productQuantity, int promotionQuantity) {
+        Receipt newReceipt = new Receipt(productName, productPrice, productQuantity, promotionQuantity);
+        receiptMap.put(productName, newReceipt);
     }
 
     public Receipt getReceipt(String productName) {
